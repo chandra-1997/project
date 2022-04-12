aa58c800a1da27230da5f80c7157e63e878ebce5
chandra sekhar patra <98772091+chandra-1997@users.noreply.github.com>
2022-04-12 15:28:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Assessment1/Header';
+import Home from './Assessment1/Home';
+
+function App() {
+  return (
+    <div>
+     <Header/>
+     <Home/>
+    </div>
+  );
+}
+
+export default App;
